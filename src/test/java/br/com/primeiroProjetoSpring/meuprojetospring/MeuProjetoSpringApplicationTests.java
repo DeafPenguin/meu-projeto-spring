83082190db67a4279f875f1b5c3bc82fa0a20542
deafpenguin <victormurilo@gmail.com>
2022-10-04 11:46:31 -0300

    Initial commit

---
+++ src/test/java/br/com/primeiroProjetoSpring/meuprojetospring/MeuProjetoSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.primeiroProjetoSpring.meuprojetospring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MeuProjetoSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
